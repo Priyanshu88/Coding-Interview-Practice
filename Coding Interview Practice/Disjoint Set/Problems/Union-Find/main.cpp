d64e8b99bdc25d606ddf33d14e8549954fa9617d
Priyanshu Pandey <2040020@sliet.ac.in>
2023-05-14 19:45:00 +0530

    changes

--- Coding Interview Practice/Disjoint Set/Problems/Union-Find/main.cpp
+++ Coding Interview Practice/Disjoint Set/Problems/Union-Find/main.cpp
@@ -1,26 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
-    public:
+public:
+    // Function to find root of a node.
 
-    //Function to find root of a node.
-    
     int find(int i, int par[], int rank1[])
     {
-    
+
         // we keep moving forward till the value of the index and the value in the array is same
-    
-        if(i == par[i]) return i;
-        par[i] = find(par[i],par,rank1);
+
+        if (i == par[i])
+            return i;
+        par[i] = find(par[i], par, rank1);
         return par[i];
     }
 
+    // Function to merge two nodes a and b.
 
-    //Function to merge two nodes a and b.
-    
-    void union_( int a, int b, int par[], int rank1[]) 
+    void union_(int a, int b, int par[], int rank1[])
     {
 
         // find root of nodes a and b.
@@ -30,42 +29,79 @@ class Solution
 
         // if they are already in same set.
 
-        if(a == b)
-        return;
+        if (a == b)
+            return;
 
-        //checking for rank, and putting in set with higher rank.
+        // checking for rank, and putting in set with higher rank.
 
-        
-        if(rank1[a] >= rank1[b])
+        if (rank1[a] >= rank1[b])
         {
-            rank1[a] += rank1[b];
+            rank1[b] += rank1[a];
             par[b] = a;
         }
 
         else
         {
-            rank1[b] += rank1[a];
+            rank1[a] += rank1[b];
             par[a] = b;
         }
         return;
-    }   
+    }
 
+    // Function to check whether 2 nodes are connected or not.
 
-    // Function to check whether 2 nodes are connected or not.  
-
-    bool isConnected(int x,int y, int par[], int rank1[])
+    bool isConnected(int x, int y, int par[], int rank1[])
     {
         int n1 = find(x, par, rank1);
         int n2 = find(y, par, rank1);
 
-    // if root of both nodes is same then they are connected.
+        // if root of both nodes is same then they are connected.
 
         return (n1 == n2);
     }
-
 };
 
+class Solution
+{
+public:
+    int find(int par[], int x)
+    {
+        if (par[x] == x)
+        {
+            return x;
+        }
+        return find(par, par[x]);
+    }
 
+    // Function to merge two nodes a and b.
+    void union_(int a, int b, int par[], int rank1[])
+    {
+        int a_parent = find(par, a);
+        int b_parent = find(par, b);
+        if (a_parent == b_parent)
+        {
+            return;
+        }
+        if (rank1[a_parent] >= rank1[b_parent])
+        {
+            rank1[a_parent] += rank1[b_parent];
+            par[b_parent] = a_parent;
+        }
+        else
+        {
+            rank1[b_parent] += rank1[a_parent];
+            par[a_parent] = b_parent;
+        }
+    }
+
+    // Function to check whether 2 nodes are connected or not.
+    bool isConnected(int x, int y, int par[], int rank1[])
+    {
+        int node1 = find(par, x);
+        int node2 = find(par, y);
+        return node1 == node2;
+    }
+};
 
 // Time Complexity: O(N + Q).
 // Expected Auxiliary Space: O(1).
